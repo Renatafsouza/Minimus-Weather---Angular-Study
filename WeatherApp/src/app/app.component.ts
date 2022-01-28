2ea3292cef6e3184c9121a64f7a1f7b850bd6e6a
Renata Francelino de Souza <renata.souza@eldorado.org.br>
2022-01-28 17:12:42 -0300

    Add layout and style on logged screen

--- WeatherApp/src/app/app.component.ts
+++ WeatherApp/src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'WeatherApp';
+  showMenu = true;
+  darkModeActive = false;
+
+  toggleMenu() {
+    console.log("toggle")
+  }
+
+  modeToggleSwitch() {
+    console.log("mode toggle switch")
+  }
 }
