c23b0113452429b7254892fe61add2469b147f7b
Renata Francelino de Souza <renata.souza@eldorado.org.br>
2022-02-02 10:12:39 -0300

    Fix background on home page

--- WeatherApp/src/app/app.module.ts
+++ WeatherApp/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -20,11 +21,13 @@ import { MenuComponent } from './shared/menu/menu.component';
     WeatherCardComponent,
     AddCardComponent,
     DetailsComponent,
-    MenuComponent
+    MenuComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
